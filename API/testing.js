2bcf1f88a293d81c502c5d9807170ad0352a2558
SebC750 <75767177+SebC750@users.noreply.github.com>
2021-05-09 21:36:34 -0400

    Create testing.js

---
+++ API/testing.js
@@ -0,0 +1,85 @@
+var express = require("express")
+var app = express();
+var util = require('./tester');
+const course = require('./courses')
+const data = course.data;
+
+const fs = require('fs')
+let rawdata = fs.readFileSync('./courses.json');
+let courseData = JSON.parse(rawdata);
+
+
+
+//Route list
+app.get('/', function(req,res)
+{
+  res.send("<h1> Welcome to the API server! </h1>");
+});
+
+
+app.get('/courses', function(req,res)
+{
+     let outputJSON =  {
+      courses: courseData['courses']}
+   res.json(outputJSON);
+   console.log("Loading data...");
+});
+
+
+
+app.get('/courses/by_code/:course_code', (req, res) =>
+{
+  
+    let query = req.params['course_code']
+    filtered_courses = courseData["courses"].filter(q => q.course_code.includes(query))
+    let outputJSON = {courses: filtered_courses}
+    res.json(outputJSON)
+    
+})
+
+
+
+app.get('/courses/by_title/:title', (req,res) =>
+{
+      let query = req.params['title']
+    filtered_courses = courseData["courses"].filter(q => q.title.includes(query))
+    let outputJSON = {courses: filtered_courses}
+    res.json(outputJSON)
+    
+      console.log("Loading data...");
+});
+
+
+
+app.get('/courses/by_instructor/:instructor', (req,res) =>
+{
+      let query = req.params['instructor']
+    filtered_courses = courseData["courses"].filter(q => q.instructor.includes(query))
+    let outputJSON = {courses: filtered_courses}
+    res.json(outputJSON)
+   
+      console.log("Loading data...");
+});
+
+
+
+app.get('/courses/by_level/:course_level', (req,res) =>
+{
+      let query = req.params['course_level']
+    filtered_courses = courseData["courses"].filter(q => q.course_level.includes(query))
+    let outputJSON = {courses: filtered_courses}
+    res.json(outputJSON)
+    
+      console.log("Loading data...");
+});
+
+
+
+
+app.use('/API',express.static('public'));
+
+app.listen(3000, function()
+{
+      console.log("Server is running.")
+      
+	})
